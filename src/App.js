9e88aab0169652b79abfbe5d7992026a9c4af2f7
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-19 22:42:28 +0100

    Changed landing page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Intro />
-      <Footer />
       </header>
     </div>
   );
--- src/Intro.js
+++ src/Intro.js
@@ -6,7 +6,18 @@ export default class Intro extends React.Component {
       <div className='intro'>
       <p>The year is 1954.</p>
       <p>Lord Astley of Marberry is dead.</p>
-      <p>You are a private investigator hired by th</p>
+      <p>You are a private investigator hired by the grieving widow to find the culprit.</p>
+      <p>You have six suspects:</p>
+      <ol>
+      <li>The wayword son.</li>
+      <li>The long suffering butler.</li>
+      <li>The maid.</li>
+      <li>The French teacher.</li>
+      <li>The best friend.</li>
+      <li>And of course, the grieving widow herself.</li>
+      </ol>
+      <hr/>
+      <p>Use your investigative skills to uncover the mystery of Marberry and find out <span className="whodunnit">"Whodunnit?"</span></p>
       </div>
       )
   }
