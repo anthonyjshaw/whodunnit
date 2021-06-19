0e0bb4f007b22e696121f34c8ec3c4665a286545
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-19 22:31:30 +0100

    Edited default react readme

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@
 import './App.css';
 import  Character from './Character';
 import Footer  from './Footer';
-import Link from './Link';
+// import Link from './Link';
+import Intro from './Intro';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          A murder!
-        </p>
-        <Character name="Marsha"/>
-        <p></p>
-        <Link href='https://www.antshaw.com'>Hi there!</Link>
+        <Intro />
       <Footer />
       </header>
     </div>
---
+++ src/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Intro extends React.Component {
+  render() {
+    return (
+      <div className='intro'>
+      <p>The year is 1954.</p>
+      <p>Lord Astley of Marberry is dead.</p>
+      <p>You are a private investigator hired by th</p>
+      </div>
+      )
+  }
+}
