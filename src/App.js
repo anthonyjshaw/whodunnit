91d4daab8055042ca644be3ab1c3e2da73ed98fb
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-20 13:00:25 +0100

    Added profile list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 import './App.css';
 import  Character from './Character';
-import Footer  from './Footer';
+// import Footer  from './Footer';
 // import Link from './Link';
 import Intro from './Intro';
 
 function App() {
   return (
-    <div className="App">
+   <div className="App">
       <header className="App-header">
         <Intro />
+        <Character />
       </header>
     </div>
   );
--- src/Character.js
+++ src/Character.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { suspects } from './suspectArray';
+
 
 class Character extends React.Component {
   render() {
+    const suspectList = suspects.map((suspect) => {
+      return (
+        <li className='profile' key={suspects.indexOf(suspect)}>{suspect.profile}</li>
+        );
+    })
     return (
-      <>
-      <p>Oh no, {this.props.name} is dead!</p>
-      <p>What should we do?</p>
-      </>
+      <div>
+      <ul>
+        {suspectList}
+      </ul>
+      </div>
       );
   }
 }
