0eb0eb72d0611f348459e13d9e80cca315b12914
Anthony Shaw <anthonyshaw@sceaga.com>
2021-09-28 16:02:31 +0100

    Added interview page styling

--- src/components/Characters/Character/Interview/Interview.js
+++ src/components/Characters/Character/Interview/Interview.js
@@ -4,17 +4,23 @@ import { removeDashes } from '../Character';
 import { addDashesToName } from '../../Characters';
 export default function Interview(props) {
     let {name} = useParams();
+    let actions = ['location', 'action', 'relationship', 'other characters'];
+    let mappedActions = actions.map(e => {
+        return <div className="interview-actions" key={actions.indexOf(e) + 1}><h2>{e}</h2></div>
+    });
     name = removeDashes(name);
     return (
         <div className="container container-fade-in-and-translate">
             <h1 className="page-header">Interview {name}</h1>
-            <div>
-                <img src={`/assets/character-avis/${addDashesToName(name)}.svg`} alt={name}/> 
-                <ul>
-                    <li className="interview-quotes">hey</li>
-                    <li className="interview-quotes">HYou</li>
-                </ul>
+            <div className="interview-container">
+                <div>
+                    <img src={`/assets/character-avis/${addDashesToName(name)}.svg`} alt={name}/> 
+                </div>
+                <div className="interview-action-wrapper">
+                    {mappedActions}
+                </div>
             </div>
+            
         </div>
     )
 }
--- src/components/Locations/Locations.js
+++ src/components/Locations/Locations.js
@@ -4,9 +4,15 @@ import { Link } from 'react-router-dom';
 
 export default function Locations() {
     const mappedLocations = locations.map(e => <Link to={`/locations/${e}`} key={locations.indexOf(e) + 1}><div className="location-card" ><h2>{e}</h2></div></Link>)
+    const carouselLocations = locations.map(e => <Link to={`/locations/${e}`} key={locations.indexOf(e)}><div className="location-carousel-card"><h2>{e}</h2></div></Link>)
     return (
         <div className="container container-fade-in-and-translate">
             <h1 className="page-header">Locations</h1>   
+            <div className="location-carousel">
+                {carouselLocations}
+                <div className="carousel__button--next"></div>
+                <div className="carousel__button--prev"></div>
+            </div>
             <div className="location-grid">
                 {mappedLocations}
             </div>
