af2ae08c18ae98fbf2165b559d51263ef80ad149
Anthony Shaw <anthonyshaw@sceaga.com>
2021-10-23 21:42:43 +0100

    Added Indie Flower font locally

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import "@fontsource/reenie-beanie"
+import "@fontsource/indie-flower";
+import "@fontsource/reenie-beanie";
 import './App.scss';
 import Routes from './components/Routes';
 
