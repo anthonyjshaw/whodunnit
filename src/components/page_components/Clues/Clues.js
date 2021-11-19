81642ed8c8845ec446d115c530fa902011e51d1a
Anthony Shaw <anthonyshaw@sceaga.com>
2021-11-19 11:48:24 +0000

    Refactored clues

--- src/components/page_components/Clues/Clues.js
+++ src/components/page_components/Clues/Clues.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Clues(props) {
+    const clues = !props.mappedClues ? 
+    <p style={{ height: 200, fontSize: '2em', textAlign: 'center' }}>
+        No clues! Why don't you <Link to="/locations">search for some clues?</Link>
+    </p> : 
+    <div className="clue-grid">{props.mappedClues}</div>;
 
     return (
         <div className='container container-fade-in-and-translate'>
             <h1 className="page-header">Clues</h1>
-            <div className="clue-grid">
-                {props.mappedClues}
-            </div>
+            {clues}
             <div style={{ height: 400 }}>
             </div>
         </div>
